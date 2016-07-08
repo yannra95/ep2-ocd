163072ab826c04958ed3527c4cd2a341b7f6b321
abelroes <abel.roes@gmail.com>
2016-07-08 16:03:11 -0300

    Alterações em Processador e Registrador
    Geração de Getters e Setters.

--- src/Processador.java
+++ src/Processador.java
@@ -20,6 +20,11 @@ public class Processador {
 		this.mbr = new Registrador("", 4, 5);
 		this.ir = new IR();
 		
+		this.ax = new Registrador("", 5, 6);
+		this.bx = new Registrador("", 7, 8);
+		this.cx = new Registrador("", 9, 10);
+		this.dx = new Registrador("", 11, 12);
+		
 		//Vetor de boolan que representa as portas e seus estados (aberta ou fechada)
 		this.barramentoDados = new boolean[23];
 		//Vetor de int que representa quais portas um dado sinal pede que sejam abertas
@@ -36,4 +41,89 @@ public class Processador {
 	 * MBR <== Memoria
 	 * IR <== MBR
 	 */
+	
+	
+
+	public IR getIr() {
+		return ir;
+	}
+
+	public void setIr(IR ir) {
+		this.ir = ir;
+	}
+
+	public Registrador getPc() {
+		return pc;
+	}
+
+	public void setPc(Registrador pc) {
+		this.pc = pc;
+	}
+
+	public Registrador getMbr() {
+		return mbr;
+	}
+
+	public void setMbr(Registrador mbr) {
+		this.mbr = mbr;
+	}
+
+	public Registrador getMar() {
+		return mar;
+	}
+
+	public void setMar(Registrador mar) {
+		this.mar = mar;
+	}
+
+	public Registrador getAx() {
+		return ax;
+	}
+
+	public void setAx(Registrador ax) {
+		this.ax = ax;
+	}
+
+	public Registrador getBx() {
+		return bx;
+	}
+
+	public void setBx(Registrador bx) {
+		this.bx = bx;
+	}
+
+	public Registrador getCx() {
+		return cx;
+	}
+
+	public void setCx(Registrador cx) {
+		this.cx = cx;
+	}
+
+	public Registrador getDx() {
+		return dx;
+	}
+
+	public void setDx(Registrador dx) {
+		this.dx = dx;
+	}
+
+	public boolean[] getBarramentoDados() {
+		return barramentoDados;
+	}
+
+	public void setBarramentoDados(boolean[] barramentoDados) {
+		this.barramentoDados = barramentoDados;
+	}
+
+	public ArrayList<Integer> getSinal() {
+		return sinal;
+	}
+
+	public void setSinal(ArrayList<Integer> sinal) {
+		this.sinal = sinal;
+	}
+	
+	
+	
 }
--- src/Registrador.java
+++ src/Registrador.java
@@ -1,9 +1,9 @@
 public class Registrador {
 
-	String conteudo;
-	int portaEntrada;
-	int portaSaida;
-	String endereco;
+	private String conteudo;
+	private int portaEntrada;
+	private int portaSaida;
+	private String endereco;
 	
 	public Registrador(String conteudo, int portaEntrada, int portaSaida,
 			String endereco) {
@@ -21,6 +21,26 @@ public class Registrador {
 	}
 	
 	
+	public String getConteudo() {
+		return conteudo;
+	}
+	public void setConteudo(String conteudo) {
+		this.conteudo = conteudo;
+	}
+	//Não possui set pois só pode ser definido na instância do objeto
+	public int getPortaEntrada() {
+		return portaEntrada;
+	}
+	//Não possui set pois só pode ser definido na instância do objeto
+	public int getPortaSaida() {
+		return portaSaida;
+	}
+	public String getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
 	
 
 }
