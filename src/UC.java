7b6629447c4e355c7281f623f8079b962c5431d8
Heloá Meneses <heloa@usp.br>
2016-07-10 00:51:43 -0300

    Implementação da leitura da instrução da memória pela UC

--- src/UC.java
+++ src/UC.java
@@ -12,7 +12,33 @@ public class UC {
 	public UC(){
 	}
 	
-	palavra = new String
+	/*
+	 * inst	reg		ind	op1				reg	ind	op2
+	 * 0000	 0	 	 0	000000000000	 0	 0	000000000000
+	 */
+	
+	public Palavra[] lerInstrucao(Memoria memoria, int endereco) {
+		String instrucao = memoria.get(endereco); // instrução a ser lida
+		
+		String linhasControle = instrucao.substring(0, 20);
+		String condJump = instrucao.substring(20,21);
+		String comandoUla = instrucao.substring(21,23);
+		String readWrite = instrucao.substring(23,24);
+		String indirecao = instrucao.substring(24,25);
+		
+		Palavra[] palavrasDeControle;
+		
+		if (indirecao.charAt(0) == '1') {
+			// Retornar mais de uma palavra
+			palavrasDeControle = new Palavra[2];
+		} else {
+			// Palavra simples
+			palavrasDeControle = new Palavra[1];
+			Palavra palavra = new Palavra(linhasControle,condJump,comandoUla,readWrite,indirecao);
+			palavrasDeControle[0] = palavra;
+		}
+		return palavrasDeControle;
+	}
 	
 	/**
 	 * Prepara a instrução recebida para criar a palavra de controle
