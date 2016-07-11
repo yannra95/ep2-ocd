fc5854c503bdf36035215003602565bb264a8880
abelroes <abel.roes@gmail.com>
2016-07-11 02:07:44 -0300

    Getter na classe Palavra

--- src/Palavra.java
+++ src/Palavra.java
@@ -1,11 +1,34 @@
 public class Palavra {
 	
-	public Palavra(String linhasDeControle, String endJump, String comandoUla, String readWrite, String indirecao) {
-		String[] palavra = new String[5];
-		palavra[0] = linhasDeControle;
-		palavra[1] = endJump;
-		palavra[2] = comandoUla;
-		palavra[3] = readWrite;
-		palavra[4] = indirecao;		
+	private String[] palavraControle;
+	
+	public Palavra(String linhasDeControle, String endereco, String comandoUla, String readWrite, String indirecao) {
+		palavraControle = new String[5];
+		palavraControle[0] = linhasDeControle;
+		palavraControle[1] = endereco;
+		palavraControle[2] = comandoUla;
+		palavraControle[3] = readWrite;
+		palavraControle[4] = indirecao;		
 	}
+
+	public String getLinhasDeControle() {
+		return palavraControle[0];
+	}
+	
+	public String getEndereco() {
+		return palavraControle[1];
+	}
+	
+	public String getComandoUla() {
+		return palavraControle[2];
+	}
+	
+	public String getReadWrite() {
+		return palavraControle[3];
+	}
+	
+	public String getIndirecao() {
+		return palavraControle[4];
+	}
+	
 }
